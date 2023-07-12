755eee6e352f86f0d05edbe73dd1f67814a3aff9
n-o-karma <nickok@comcast.net>
2023-07-12 12:36:06 -0400

    7/12 morning edits

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,23 +4,29 @@ csvpath = os.path.join("Resources" , "budget_data.csv")
 
 with open(csvpath) as csvfile:
         csvreader = csv.reader(csvfile, delimiter=",")
-        next(csvreader) #skip header row
+        next(csvreader)         #skip header row
         count = 0
         total = 0
         change1 = 0
-        change2 = 0
-        delta = 0
+        change2 = change1
+
+        changelist = []
         for row in csvreader:
             count += 1
             row[1] = int(row[1])
-            totalchange = row[1]
-            change1= totalchange
-            total += totalchange
-        for row in csvreader:
+            change1= row[1]
+            total += row[1]
+            if count != 1:      #skip the first month; there was no change
+                changelist.append(change1-change2)
+            # for i in range(len(changelist) -1):
+            #      if changelist[i+1] > changelist[i]:
+            #          tempmax = row[0]
             change2 = row[1]
-            delta = change2+change1
-            print(f"{delta} \n")    
-            
-        print(count)
-        print(total)
-            
+                    
+        print(f"Financial Analysis \n ----------------------------")    
+        print(f"Total Months: {count}")
+        print(f"Total: ${total}")
+        print(f"Average Change: ${round(sum(changelist)/len(changelist),2)}")
+        print(f"Greatest Increase in Profits: $ {max(changelist)}")
+        print(f"Greatest Decrease in Profits: $ {min(changelist)}")
+        print(tempmax)
