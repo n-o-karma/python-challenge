8c44dab7b0fa0e425ccffbd6b9817f2746789c55
n-o-karma <nickok@comcast.net>
2023-07-12 00:33:12 -0400

    End of night 1

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,26 @@
 import os
 import csv
-csvpath = os.path.join("Resources", "election_data.csv")
+csvpath = os.path.join("Resources" , "budget_data.csv")
+
 with open(csvpath) as csvfile:
         csvreader = csv.reader(csvfile, delimiter=",")
+        next(csvreader) #skip header row
         count = 0
+        total = 0
+        change1 = 0
+        change2 = 0
+        delta = 0
         for row in csvreader:
             count += 1
+            row[1] = int(row[1])
+            totalchange = row[1]
+            change1= totalchange
+            total += totalchange
+        for row in csvreader:
+            change2 = row[1]
+            delta = change2+change1
+            print(f"{delta} \n")    
+            
         print(count)
+        print(total)
             
